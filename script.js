898ddafc047a8159637e33add800c3dbf1678092
A00383 <a00383@mariareinamarianistas.net>
2025-09-07 17:17:47 -0500

    Advancing on the pop-up capability, pop up opening and closing capabilities have been augmented in order for the pop up to close when clicking outside of it

--- script.js
+++ script.js
@@ -17,3 +17,127 @@ popup.addEventListener('click', (e) => {
         popup.classList.remove('active');
     }
 });
+const popupname = document.getElementById('pop-up-name');
+const mainorganelname = document.getElementById('organelo');
+const membranacelular = document.getElementById ('membrana celular');
+const citoplasma = document.getElementById ('citoplasma');
+const nucleolo = document.getElementById ('nucleolo');
+const nucleo = document.getElementById ('nucleo');
+const reticuloendoplasmatico = document.getElementById ('reticulo endoplasmatico');
+const centriolos = document.getElementById ('centriolos');
+const microtubulos = document.getElementById ('microtubulos');
+const mitocondrias = document.getElementById ('mitocondrias');
+const lisosomas = document.getElementById ('lisosomas');
+const aparatodegolgi = document.getElementById ('aparato de golgi');
+
+//membrana celular
+membranacelular.addEventListener('click', () => {
+    popupname.textContent = "Membrana celular";
+})
+
+membranacelular.addEventListener('mouseenter', () => {
+    mainorganelname.textContent = "Membrana celular";
+})
+membranacelular.addEventListener('mouseleave', () => {
+    mainorganelname.textContent = "Célula animal";
+});
+
+//citoplasma
+citoplasma.addEventListener('click', () => {
+    popupname.textContent = "Citoplasma";
+})
+citoplasma.addEventListener('mouseenter', () => {
+    mainorganelname.textContent = "Citoplasma";
+})
+citoplasma.addEventListener('mouseleave', () => {
+    mainorganelname.textContent = "Célula animal";
+});
+
+//nucleolo
+nucleolo.addEventListener('click', () => {
+    popupname.textContent = "Nucléolo";
+})
+nucleolo.addEventListener('mouseenter', () => {
+    mainorganelname.textContent = "Nucléolo";
+})
+nucleolo.addEventListener('mouseleave', () => {
+    mainorganelname.textContent = "Célula animal";
+});
+
+//nucleo
+nucleo.addEventListener('click', () => {
+    popupname.textContent = "Núcleo";
+})
+nucleo.addEventListener('mouseenter', () => {
+    mainorganelname.textContent = "Núcleo";
+})
+nucleo.addEventListener('mouseleave', () => {
+    mainorganelname.textContent = "Célula animal";
+});
+
+//retículo endoplasmático
+reticuloendoplasmatico.addEventListener('click', () => {
+    popupname.textContent = "Retículo endplasmático";
+})
+reticuloendoplasmatico.addEventListener('mouseenter', () => {
+    mainorganelname.textContent = "Retículo endoplasmático";
+})
+reticuloendoplasmatico.addEventListener('mouseleave', () => {
+    mainorganelname.textContent = "Célula animal";
+});
+
+//centriolos
+centriolos.addEventListener('click', () => {
+    popupname.textContent = "Centriolos";
+})
+centriolos.addEventListener('mouseenter', () => {
+    mainorganelname.textContent = "Centriolos";
+})
+centriolos.addEventListener('mouseleave', () => {
+    mainorganelname.textContent = "Célula animal";
+});
+
+//microtubulos
+microtubulos.addEventListener('click', () => {
+    popupname.textContent = "Microtúbulos";
+})
+microtubulos.addEventListener('mouseenter', () => {
+    mainorganelname.textContent = "Microtúbulos";
+})
+microtubulos.addEventListener('mouseleave', () => {
+        mainorganelname.textContent = "Célula animal";
+});
+
+//mitocondrias
+mitocondrias.addEventListener('click', () => {
+    popupname.textContent = "Mitocondrias";
+})
+mitocondrias.addEventListener('mouseenter', () => {
+    mainorganelname.textContent = "Mitocondrias";
+})
+mitocondrias.addEventListener('mouseleave', () => {
+    mainorganelname.textContent = "Célula animal";
+    });
+
+//lisosomas
+lisosomas.addEventListener('click', () => {
+    popupname.textContent = "Lisosomas";
+})
+lisosomas.addEventListener('mouseenter', () => {
+    mainorganelname.textContent = "Lisosomas";
+})
+lisosomas.addEventListener('mouseleave', () => {
+    mainorganelname.textContent = "Célula animal";
+    });
+
+//aparato de golgi
+aparatodegolgi.addEventListener('click', () => {
+    popupname.textContent = "Aparato de Golgi";
+})
+aparatodegolgi.addEventListener('mouseenter', () => {
+    mainorganelname.textContent = "Aparato de Golgi";
+})
+aparatodegolgi.addEventListener('mouseleave', () => {
+    mainorganelname.textContent = "Célula animal";
+});
+

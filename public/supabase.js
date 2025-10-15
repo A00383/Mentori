4e7be28cfff2b45c2b0eab3eb6ee8001a1df72da
A00383 <a00383@mariareinamarianistas.net>
2025-10-14 23:44:58 -0500

    Final ver of all scripts

--- public/supabase.js
+++ public/supabase.js
@@ -3,4 +3,4 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 const supabaseUrl = "https://ycbujchvtgfbxiqexdeo.supabase.co"
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InljYnVqY2h2dGdmYnhpcWV4ZGVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgxNjE1NDEsImV4cCI6MjA3MzczNzU0MX0.Ruo65SqzlRDGSIf3w5-juU0aJUCLXuzqPl6ZAog4pyQ"
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+window.supabase = createClient(supabaseUrl, supabaseAnonKey);

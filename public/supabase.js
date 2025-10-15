5b666bd11a806e009a7ac0705344ae1f05ee9b50
A00383 <a00383@mariareinamarianistas.net>
2025-10-15 02:24:43 -0500

    Final ver of all scripts

--- public/supabase.js
+++ public/supabase.js
@@ -5,4 +5,9 @@ export const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJ
 
 export const IMGS_BUCKET = "CelulaAnimalImgs";
 
+console.log("Supabase URL:", supabaseUrl);
+console.log("Supabase key:", supabaseAnonKey);
+console.log("Types:", typeof supabaseUrl, typeof supabaseAnonKey);
+
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
